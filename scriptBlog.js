054333c59cbee5922375a61e875b5f1de1de033f
eme e <elhabuelosimpson@gmail.com>
2025-03-20 12:21:30 +0100

    Filtros

--- scriptBlog.js
+++ scriptBlog.js
@@ -7,6 +7,8 @@ const body = document.body
 const toggleLeft = document.querySelector(".toggle__left")
 const toggleRight = document.querySelector(".toggle__right")
 
+const likeBtn = document.querySelector(".like__btn")
+
 const cardContainer = document.querySelector(".cards__container")
 const posts = [
   {
@@ -301,6 +303,7 @@ colorMode.addEventListener("click", () => {
 });
 
 
+
 commentBtn.addEventListener("click", async (event) => {
     
     event.preventDefault();
@@ -347,33 +350,12 @@ commentBtn.addEventListener("click", async (event) => {
     userComment.appendChild(paragraph)
     commentCard.appendChild(userComment)
 
-    const commentIcons = document.createElement("div")
-    commentIcons.classList.add("comment__icons")
-
-    const replyBtn = document.createElement("button")
-    replyBtn.classList.add("reply__btn")
-    replyBtn.innerHTML = `<svg class="comment__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 2H8C4.691 2 2 4.691 2 8v12a1 1 0 0 0 1 1h13c3.309 0 6-2.691 6-6V8c0-3.309-2.691-6-6-6zm4 13c0 2.206-1.794 4-4 4H4V8c0-2.206 1.794-4 4-4h8c2.206 0 4 1.794 4 4v7z"></path><circle cx="9.5" cy="11.5" r="1.5"></circle><circle cx="14.5" cy="11.5" r="1.5"></circle></svg>`
-
-    const likeBtn = document.createElement("button")
-    likeBtn.classList.add("like__btn")
-    likeBtn.innerHTML = `<svg class="like__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"></path></svg>`
-
-    commentCard.appendChild(commentIcons)
-    commentIcons.appendChild(replyBtn)
-    commentIcons.appendChild(likeBtn)
-
     commentsContainer.appendChild(commentCard)
 
 
     textArea.value = ""
 
-    
 })
-
-
-
-  
-
 
 function createPost () {
 
